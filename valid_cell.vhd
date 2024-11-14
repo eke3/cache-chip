43d6e258b71c4c418a923961a70288af65cdac70
Eric E <ekeyeric11@gmail.com>
2024-11-14 15:57:48 -0500

    small fix for valid_cell.vhd

--- valid_cell.vhd
+++ valid_cell.vhd
@@ -18,7 +18,7 @@ architecture Structural of valid_cell is
     component cache_cell is
         port (
             write_data  : in  std_logic; -- 1-bit write data
-            reset : in std_logic;
+            reset       : in  std_logic;
             chip_enable : in  std_logic; -- 1-bit chip enable
             RW          : in  std_logic; -- 1-bit Read/Write signal
             read_data   : out std_logic  -- 1-bit read data
@@ -43,20 +43,13 @@ architecture Structural of valid_cell is
         );
     end component inverter;
 
-    signal sel_not, reset_not, write_data_mux_out, rw_mux_out : STD_LOGIC;
+    signal reset_not, write_data_mux_out, rw_mux_out : STD_LOGIC;
 
-    for sel_inverter, reset_inverter: inverter use entity work.inverter(structural);
+    for reset_inverter: inverter use entity work.inverter(structural);
     for write_data_mux, rw_mux: mux_2x1 use entity work.mux_2x1(structural);
     for cache_cell_inst: cache_cell use entity work.cache_cell(structural);
 
 begin
-
-    -- Instantiate the inverter to generate sel_not signal
-    sel_inverter: component inverter
-    port map (
-        input       => sel,
-        output      => sel_not
-    );
 
     -- Instantiate the inverter to generate reset_not signal
     reset_inverter: component inverter
@@ -86,7 +79,7 @@ begin
         write_data  => write_data_mux_out,
         chip_enable => chip_enable,
         RW          => rw_mux_out,
-        reset => reset,
+        reset       => reset,
         read_data   => read_data
     );
 
