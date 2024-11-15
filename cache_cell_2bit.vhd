6d609b884528b169c50ff247d08e348f633856cc
Eric E <ekeyeric11@gmail.com>
2024-11-14 19:25:20 -0500

    rolled back 2 and 8 bit cache cells and vectors

--- cache_cell_2bit.vhd
+++ cache_cell_2bit.vhd
@@ -4,12 +4,11 @@
 
 library STD;
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.std_logic_1164.all;
 
 entity cache_cell_2bit is
     port (
         write_data  : in  std_logic_vector(1 downto 0);
-        reset       : in  std_logic;               -- New reset input
         chip_enable : in  std_logic;
         RW          : in  std_logic;
         read_data   : out std_logic_vector(1 downto 0)
@@ -20,7 +19,6 @@ architecture structural of cache_cell_2bit is
     component cache_cell is
         port (
             write_data  : in  std_logic;
-            reset       : in  std_logic;           -- Reset input for each cache cell instance
             chip_enable : in  std_logic;
             RW          : in  std_logic;
             read_data   : out std_logic
@@ -31,22 +29,20 @@ architecture structural of cache_cell_2bit is
     for cache_0, cache_1: cache_cell use entity work.cache_cell(structural);
 
 begin
-    -- Map signals to each cache cell instance
-    cache_0: cache_cell port map (
-        write_data  => write_data(0),
-        reset       => reset,              -- Connect reset to cache_0
-        chip_enable => chip_enable,
-        RW          => RW,
-        read_data   => read_data(0)
-    );
 
-    cache_1: cache_cell port map (
-        write_data  => write_data(1),
-        reset       => reset,              -- Connect reset to cache_1
-        chip_enable => chip_enable,
-        RW          => RW,
-        read_data   => read_data(1)
+    cache_0: component cache_cell
+    port map (
+        write_data(0),
+        chip_enable,
+        RW,
+        read_data(0)
+    );
+    cache_1: component cache_cell
+    port map (
+        write_data(1),
+        chip_enable,
+        RW,
+        read_data(1)
     );
 
 end architecture structural;
-
