ca41f3a2f18468e9e300bd3d13b852376e0476e2
Eric E <ekeyeric11@gmail.com>
2024-11-14 20:08:33 -0500

    valid cell working

--- valid_cell_tb.vhd
+++ valid_cell_tb.vhd
@@ -1,4 +1,3 @@
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.all;
 use IEEE.STD_LOGIC_TEXTIO.all;
@@ -54,53 +53,53 @@ begin
     stimulus_process: process
     begin
         -- Test Case 1: Apply reset = 1 (Expect read_data = 0)
-        reset <= '1';
-        write_data <= '1';
+        reset       <= '1';
+        write_data  <= '1';
         chip_enable <= '1';
-        RW <= '1';
+        RW          <= '1';
         wait for 10 ns;
         print_output;
 
         -- Test Case 2: Release reset and write 1 to the cell
-        reset <= '0';
-        write_data <= '1';
+        reset       <= '0';
+        write_data  <= '1';
         chip_enable <= '1';
-        RW <= '0';  -- Write mode
+        RW          <= '0'; -- Write mode
         wait for 10 ns;
         print_output;
 
         -- Test Case 3: Read the stored value (Expect read_data = 1)
-        RW <= '1';  -- Read mode
+        RW          <= '1'; -- Read mode
         wait for 10 ns;
         print_output;
 
         -- Test Case 4: Set reset to 1 to clear the data
-        reset <= '1';
+        reset       <= '1';
         wait for 10 ns;
         print_output;
 
         -- Test Case 5: Release reset and attempt to write 0
-        reset <= '0';
-        write_data <= '0';
-        RW <= '0';  -- Write mode
+        reset       <= '0';
+        write_data  <= '0';
+        RW          <= '0'; -- Write mode
         wait for 10 ns;
         print_output;
 
         -- Test Case 6: Read the cleared value (Expect read_data = 0)
-        RW <= '1';  -- Read mode
+        RW          <= '1'; -- Read mode
         wait for 10 ns;
         print_output;
 
         -- Test Case 7: Disable chip_enable and attempt to write 1 (Expect no write)
         chip_enable <= '0';
-        write_data <= '1';
-        RW <= '0';  -- Write mode
+        write_data  <= '1';
+        RW          <= '0'; -- Write mode
         wait for 10 ns;
         print_output;
 
         -- Test Case 8: Enable chip and read to check no write occurred
         chip_enable <= '1';
-        RW <= '1';  -- Read mode
+        RW          <= '1'; -- Read mode
         wait for 10 ns;
         print_output;
 
