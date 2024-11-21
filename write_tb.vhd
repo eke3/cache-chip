fe2d001991116f34a4422b5b32fe1e1b3138943d
Eric E <ekeyeric11@gmail.com>
2024-11-20 23:19:12 -0500

    fix typo

--- write_tb.vhd
+++ write_tb.vhd
@@ -248,7 +248,7 @@ architecture test of write_tb is
         -- Now connect everything to the cache array
 
         cache: entity work.block_cache(structural)
-            port map (mem_data => mem_data, mem_addr => mem_addr, hit_miss => hit_miss_reg, R_W => RW_cache, byte_offset => byte_decoder_reg, block_offset => block_decoder_reg, cpu_data => data_reg2, read_data => read_cache);
+            port map (mem_data => mem_data, hit_miss => hit_miss_reg, R_W => RW_cache, byte_offset => byte_decoder_reg, block_offset => block_decoder_reg, cpu_data => data_reg2, read_data => read_cache);
            
         -- register for read data
         
