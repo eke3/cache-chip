ccf32cb919ad89fa161bdd935c94ae0d2d71c124
Eric E <ekeyeric11@gmail.com>
2024-11-29 22:12:12 -0500

    Revert "trimmed concatenator_8bit.vhd"
    This reverts commit bfc56ab006d1a1b4d3961cb70cab6c4da8cfaad5.

--- src/mux_demux/mux_16x1_8bit.vhd
+++ src/mux_demux/mux_16x1_8bit.vhd
@@ -15,37 +15,60 @@ architecture Structural of mux_16x1_8bit is
     -- Declare the 2x1 8-bit multiplexer component
     component mux_16x1 is
         port (
-            inputs      : in  STD_LOGIC_VECTOR(15 downto 0); -- 16-bit input vector
-            sel         : in  STD_LOGIC_VECTOR(3 downto 0);  -- 4-bit select signal
+            inputs      : in  STD_LOGIC_VECTOR(15 downto 0);        -- 16-bit input vector
+            sel         : in  STD_LOGIC_VECTOR(3 downto 0);         -- 4-bit select signal
             sel_one_hot : in  std_logic_vector(15 downto 0);
-            output      : out STD_LOGIC                      -- Output of the multiplexer
+            output      : out STD_LOGIC                             -- Output of the multiplexer
         );
     end component mux_16x1;
+
+    component concatenator_8bit is
+        port (
+            in0    : in  std_logic;                                 -- First 2-bit input
+            in1    : in  std_logic;                                 -- Second 2-bit input
+            in2    : in  std_logic;                                 -- First 2-bit input
+            in3    : in  std_logic;                                 -- Second 2-bit input
+            in4    : in  std_logic;                                 -- First 2-bit input
+            in5    : in  std_logic;                                 -- Second 2-bit input
+            in6    : in  std_logic;                                 -- First 2-bit input
+            in7    : in  std_logic;                                 -- Second 2-bit input
+            in8    : in  std_logic;                                 -- First 2-bit input
+            in9    : in  std_logic;                                 -- Second 2-bit input
+            in10   : in  std_logic;                                 -- First 2-bit input
+            in11   : in  std_logic;                                 -- Second 2-bit input
+            in12   : in  std_logic;                                 -- First 2-bit input
+            in13   : in  std_logic;                                 -- Second 2-bit input
+            in14   : in  std_logic;                                 -- First 2-bit input
+            in15   : in  std_logic;                                 -- Second 2-bit input
+            output : out std_logic_vector(15 downto 0)              -- Concatenated 4-bit output
+        );
+    end component concatenator_8bit;
 
     signal bits : std_logic_vector(127 downto 0);
 
 begin
 
     gen_1: for i in 0 to 7 generate
-        bits((16 * (i + 1) - 1) downto (16 * i)) <=
-            (
-                inputs(i + 120),
-                inputs(i + 112),
-                inputs(i + 104),
-                inputs(i + 96),
-                inputs(i + 88),
-                inputs(i + 80),
-                inputs(i + 72),
-                inputs(i + 64),
-                inputs(i + 56),
-                inputs(i + 48),
-                inputs(i + 40),
-                inputs(i + 32),
-                inputs(i + 24),
-                inputs(i + 16),
-                inputs(i + 8),
-                inputs(i)
-            );
+        concat: entity work.concatenator_8bit(Structural)
+        port map (
+            in0         => inputs(i),
+            in1         => inputs(i + 8),
+            in2         => inputs(i + 16),                          -- First 2-bit input
+            in3         => inputs(i + 24),                          -- Second 2-bit input
+            in4         => inputs(i + 32),                          -- First 2-bit input
+            in5         => inputs(i + 40),                          -- Second 2-bit input
+            in6         => inputs(i + 48),                          -- First 2-bit input
+            in7         => inputs(i + 56),                          -- Second 2-bit input
+            in8         => inputs(i + 64),                          -- First 2-bit input
+            in9         => inputs(i + 72),                          -- Second 2-bit input
+            in10        => inputs(i + 80),                          -- First 2-bit input
+            in11        => inputs(i + 88),                          -- Second 2-bit input
+            in12        => inputs(i + 96),                          -- First 2-bit input
+            in13        => inputs(i + 104),                         -- Second 2-bit input
+            in14        => inputs(i + 112),                         -- First 2-bit input
+            in15        => inputs(i + 120),                         -- Second 2-bit input
+            output      => bits((16 * (i + 1) - 1) downto (16 * i)) -- Concatenated 4-bit output
+        );
     end generate;
 
     gen_2: for i in 0 to 7 generate
---
+++ src/util/concatenator_8bit.vhd
@@ -0,0 +1,34 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+-- Entity for concatenator
+entity concatenator_8bit is
+    port (
+        in0    : in  std_logic;
+        in1    : in  std_logic;
+        in2    : in  std_logic;
+        in3    : in  std_logic;
+        in4    : in  std_logic;
+        in5    : in  std_logic;
+        in6    : in  std_logic;
+        in7    : in  std_logic;
+        in8    : in  std_logic;
+        in9    : in  std_logic;
+        in10   : in  std_logic;
+        in11   : in  std_logic;
+        in12   : in  std_logic;
+        in13   : in  std_logic;
+        in14   : in  std_logic;
+        in15   : in  std_logic;
+        output : out std_logic_vector(15 downto 0)
+    );
+end entity concatenator_8bit;
+
+-- Architecture using Structural approach
+architecture Structural of concatenator_8bit is
+
+begin
+
+    output <= (in15, in14, in13, in12, in11, in10, in9, in8, in7, in6, in5, in4, in3, in2, in1, in0);
+
+end architecture Structural;
