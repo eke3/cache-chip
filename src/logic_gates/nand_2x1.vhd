8d790f90fb20cbdc63be49d77bbab6fa3885d053
Eric E <ekeyeric11@gmail.com>
2024-12-03 10:15:01 -0500

    took inverters out of nand & nor

--- src/logic_gates/nand_2x1.vhd
+++ src/logic_gates/nand_2x1.vhd
@@ -13,45 +13,10 @@ entity nand_2x1 is
 end nand_2x1;
 
 architecture Structural of nand_2x1 is
-    -- Declare the and_2x1 component
-    component and_2x1
-        port (
-            A      : in  STD_LOGIC;
-            B      : in  STD_LOGIC;
-            output : out STD_LOGIC
-        );
-    end component;
-
-    -- Declare the inverter component
-    component inverter
-        port (
-            input  : in  STD_LOGIC;
-            output : out STD_LOGIC
-        );
-    end component;
-
-    -- Intermediate signal
-    signal and_out : STD_LOGIC;
-
-    -- Use the work library components
-    for and_gate: and_2x1 use entity work.and_2x1(Structural);
-    for inv_gate: inverter use entity work.inverter(Structural);
 
 begin
-    -- Instantiate the and_2x1 gate to calculate A and B
-    and_gate: and_2x1
-    port map (
-        A      => A,
-        B      => B,
-        output => and_out
-    );
 
-    -- Instantiate the inverter to invert the output of the and_2x1 gate
-    inv_gate: inverter
-    port map (
-        input  => and_out,
-        output => output
-    );
+    output <= A nand B;
 
 end Structural;
 
--- src/logic_gates/nor_2x1.vhd
+++ src/logic_gates/nor_2x1.vhd
@@ -11,46 +11,9 @@ end nor_2x1;
 
 architecture Structural of nor_2x1 is
 
-    component inverter
-        port (
-            input  : in  std_logic;     -- Input
-            output : out std_logic      -- Output
-        );
-    end component;
-
-    component or_2x1
-        port (
-            A      : in  std_logic;     -- First input
-            B      : in  std_logic;     -- Second input
-            output : out std_logic      -- OR output
-        );
-    end component;
-
-    signal or_out, nor_out : std_logic; -- Intermediate signals
-
-    -- Use the work library components
-    for or_gate: or_2x1 use entity work.or_2x1(Structural);
-    for inverter_gate: inverter use entity work.inverter(Structural);
-
 begin
 
-    -- Instantiate the or_2x1 gate to calculate A or B
-    or_gate: or_2x1
-    port map (
-        A      => A,
-        B      => B,
-        output => or_out
-    );
-
-    -- Instantiate the inverter to invert the output of the or_2x1 gate
-    inverter_gate: inverter
-    port map (
-        input  => or_out,
-        output => nor_out
-    );
-
-    -- The final output is the inverted OR result (NOR)
-    output <= nor_out;
+    output <= A nor B;
 
 end Structural;
 
