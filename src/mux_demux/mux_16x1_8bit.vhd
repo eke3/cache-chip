497c890173b211e8be42d75b739df4c16035ea7f
Eric E <ekeyeric11@gmail.com>
2024-12-03 00:56:06 -0500

    trims concatenator

--- src/mux_demux/mux_16x1_8bit.vhd
+++ src/mux_demux/mux_16x1_8bit.vhd
@@ -22,57 +22,33 @@ architecture Structural of mux_16x1_8bit is
         );
     end component;
 
-    component concatenator_8bit
-        port (
-            in0    : in  std_logic;                                 -- First input bit
-            in1    : in  std_logic;                                 -- Second input bit
-            in2    : in  std_logic;                                 -- Third input bit
-            in3    : in  std_logic;                                 -- Fourth input bit
-            in4    : in  std_logic;                                 -- Fifth input bit
-            in5    : in  std_logic;                                 -- Sixth input bit
-            in6    : in  std_logic;                                 -- Seventh input bit
-            in7    : in  std_logic;                                 -- Eighth input bit
-            in8    : in  std_logic;                                 -- Ninth input bit
-            in9    : in  std_logic;                                 -- Tenth input bit
-            in10   : in  std_logic;                                 -- Eleventh input bit
-            in11   : in  std_logic;                                 -- Twelfth input bit
-            in12   : in  std_logic;                                 -- Thirteenth input bit
-            in13   : in  std_logic;                                 -- Fourteenth input bit
-            in14   : in  std_logic;                                 -- Fifteenth input bit
-            in15   : in  std_logic;                                 -- Sixteenth input bit
-            output : out std_logic_vector(15 downto 0)              -- Concatenated 16-bit output
-        );
-    end component;
-
     signal bits : std_logic_vector(127 downto 0);
 
     -- Bind components
     for all : mux_16x1 use entity work.mux_16x1(Structural);
-    for all : concatenator_8bit use entity work.concatenator_8bit(Structural);
 
 begin
 
-    -- Generate block for concatenators
+    -- Generate block for routing wires directly
     gen_1: for i in 0 to 7 generate
-        concat: concatenator_8bit
-        port map (
-            in0         => inputs(i),
-            in1         => inputs(i + 8),
-            in2         => inputs(i + 16),
-            in3         => inputs(i + 24),
-            in4         => inputs(i + 32),
-            in5         => inputs(i + 40),
-            in6         => inputs(i + 48),
-            in7         => inputs(i + 56),
-            in8         => inputs(i + 64),
-            in9         => inputs(i + 72),
-            in10        => inputs(i + 80),
-            in11        => inputs(i + 88),
-            in12        => inputs(i + 96),
-            in13        => inputs(i + 104),
-            in14        => inputs(i + 112),
-            in15        => inputs(i + 120),
-            output      => bits((16 * (i + 1) - 1) downto (16 * i))
+        -- Route wires directly to output
+        bits((16 * (i + 1) - 1) downto (16 * i)) <= (
+            inputs(i + 120), 
+            inputs(i + 112), 
+            inputs(i + 104), 
+            inputs(i + 96), 
+            inputs(i + 88), 
+            inputs(i + 80), 
+            inputs(i + 72), 
+            inputs(i + 64), 
+            inputs(i + 56), 
+            inputs(i + 48), 
+            inputs(i + 40), 
+            inputs(i + 32), 
+            inputs(i + 24), 
+            inputs(i + 16), 
+            inputs(i + 8), 
+            inputs(i)
         );
     end generate;
 
